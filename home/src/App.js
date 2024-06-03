c4be67ffe221373a630c87ea7c3b6e51d6314216
calebmutisya <mutisyacalebmusau@gmail.com>
2024-06-03 19:05:01 +0300

    practice

--- home/src/App.js
+++ home/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className='w80 centerhorizontally h-20 bgpink mauto spacebetween'>
+        <div className='h64 centerhorizontally bgwhite'>
+          Logo
+        </div>
+        <div className='flex'>
+          <ul className='flex'>
+            <li className='p-x10 bggrey h64 centerhorizontally'>Home</li>
+            <li className='p-x10 bggrey h64 centerhorizontally'>Properties</li>
+            <li className='p-x10 bggrey h64 centerhorizontally'>Why Us</li>
+            <li className='p-x10 bggrey h64 centerhorizontally'>About Us</li>
+          </ul>
+        </div>
+        <div className='flex'>
+          <button className='navbutton h64 centerhorizontally m-x20'>Sign Up</button>
+          <button className='navbutton h64 centerhorizontally m-x20'>Login</button>
+        </div>
+      </nav>
     </div>
   );
 }
